d4cfad7801b7f00c2520d49261c8190793f49a8e
Vitor Oliveira <104097532+OliveiraHackerSecurity@users.noreply.github.com>
2022-04-25 05:25:37 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,14 @@
+# Atualizar
+apt update -y
+apt upgrade -y
+
+# Instalar programas necessário
+apt install nano -y
+apt install python -y
+apt install python2 -y
+apt install python3 -y
+
+# Instalar pacotes necessário
+pip install email
+pip install imaplib
+
