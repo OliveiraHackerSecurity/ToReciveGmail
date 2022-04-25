f7a6c8c871035288bf43b6adb3f4a851073a26bc
Vitor Oliveira <104097532+OliveiraHackerSecurity@users.noreply.github.com>
2022-04-25 05:50:36 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,14 +1,15 @@
 import email
 import imaplib
 
-EMAIL = '*** SEU EMA DO GMAIL ***'
-PASSWORD = '*** SUA SENHA ***'
+EMAIL = '** Seu email **'
+PASSWORD = '** Senha email **'
 SERVER = 'imap.gmai.com'
-# Monar a conexão #
+# Montar a conexão #
 mail = imaplib.IMAP_SSL(SERVER)
 mail.login(EMAIL, PASSWORD)
 mail.select('inbox')
-status, data = mail.search(None, 'ALL') mail_ids []
+status, data = mail.search(None, 'ALL')
+mail_ids []
 
 for block in data:
   mail_ids += block.splint()
@@ -18,7 +19,7 @@ for i um mail_ids:
     status, data = mail.fetch(i, '(RFCC822)')
     for response_part in data:
         if isinstance(response_part, tuple):
-            message = email.message_from_bytes(response_part[1])
+            message = email.message_from_bytes(response_part[>
 
             mail_from = message['from']
             mail_subject = message['subject']
@@ -26,7 +27,7 @@ for i um mail_ids:
             if message.is_multipart():
                 mail_content = ''
                 for part in message.get_payload():
-                    if part.get_content_type() == 'text/plain':
+                    if part.get_content_type() == 'text/plain>
                         mail_content += party.get_payload()
 else:
     mail_content = message.get_payload()
